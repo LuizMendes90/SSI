1c0397f823efad6dc023d32e9c54de3055617fc6
Luiz Mendes <luiz.mendes@outlook.com>
2018-11-18 21:20:13 -0200

    versao final

--- index.php
+++ index.php
@@ -4,14 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>SSI</title>
 </head>
-<body>
-    Segurança de sistemas de informação
-    <form action="login.php" method="get">
-        <input type="text" name="login" id="">
-        <input type="password" name="senha" id="">
-        <input type="submit" value="Enviar">
+<body class="text-center">
+<div class="container col-md-4">
+
+    <form class="form-signin" action="login.php" method="get">
+      <img class="mb-4" src="../../assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+      <h1 class="h3 mb-3 font-weight-normal">SSI - Trabalho KALI</h1>
+      <label for="inputEmail" class="sr-only">Usuário</label>
+      <input type="text" name='login' id="inputEmail" class="form-control" placeholder="Usuário" required="" autofocus="">
+      <label for="inputPassword"  class="sr-only">Senha</label>
+      <input type="password" name='senha' id="inputPassword" class="form-control" placeholder="Senha" required="">
+      <div class="checkbox mb-3">
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
     </form>
+    </div>  
+
 </body>
 </html>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
